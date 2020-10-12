276eac2f97b20b8340b8338df1bb379f0685f67d
Joshua Shevitz <joshshev@yahoo.com>
2020-10-12 19:04:40 -0400

    pushing the pages i made and their layouts

---
+++ app/src/main/java/com/example/myapplication/GroceryList.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroceryList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grocery_list)
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        new_list_btn_guest.setOnClickListener{
+            val intent = Intent(this, GroceryList::class.java)
+
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+
+        sign_in_btn.setOnClickListener{
+            val intent = Intent(this, SignIn::class.java)
+
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+
+        sign_up_btn.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+
+
+    }
+    companion object {
+        const val RESPOND_CODE = 1
+        const val REQUEST_CODE = 1
     }
 }
